8f187eeec8e570569cdeb05ed7352a37efc1f0da
Erdoo <erdoo.kuhwa@gmail.com>
2021-04-08 10:46:19 -0500

    added cart icon & implemented cart reducer

---
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import CustomButton from '../custom-button/custom-button.component';
+
+import './cart-dropdown.styles.scss';
+
+
+const CartDropdown = () => (
+    <div className='cart-dropdown'>
+        <div className='cart-items'>
+            <CustomButton>PROCEED TO CHECKOUT</CustomButton>
+        </div>
+    </div>
+)
+
+export default CartDropdown;
--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -3,11 +3,13 @@ import { Link } from 'react-router-dom';
 import { ReactComponent as Logo } from '../../assets/crown.svg';
 import { auth } from '../firebase/firebase.utils';
 import { connect } from 'react-redux';
+import CartIcon from '../cart-icon/cart-icon.component';
+import CartDropdown from '../cart-dropdown/cart-dropdown.component';
 
 import './header.styles.scss';
 
 
-const Header = ({ currrentUser }) => (
+const Header = ({ currrentUser, hidden }) => (
     <div className='header'>
         <Link to='/' className='logo-container' >
             <Logo className='logo' />
@@ -28,12 +30,17 @@ const Header = ({ currrentUser }) => (
             ) : (
                 <Link className='option' to='/signin'> SIGN IN </Link>
             )}
+            <CartIcon />
         </div>
+    
+        { hidden ? null : <CartDropdown /> }
+
     </div>
 );
 
-const mapStateToProps = state => ({
-    currrentUser: state.user.currentUser
+const mapStateToProps = ({ user: {currentUser}, cart: {hidden} }) => ({
+    currentUser,
+    hidden
 })
 
 export default connect(mapStateToProps)(Header);
